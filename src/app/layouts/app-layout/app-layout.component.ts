f8295df5670477fe29cb3e77ac2204eb6cb356c5
Arthur <arthur.cheng@signature-code.com>
2021-01-27 18:25:17 +0100

    notification pop up on new notif

--- src/app/layouts/app-layout/app-layout.component.ts
+++ src/app/layouts/app-layout/app-layout.component.ts
@@ -6,6 +6,7 @@ import { WebsocketConnection } from 'src/modules/common/WebsocketConnection';
 import { NotificationService } from 'src/modules/notification/services/notification.service';
 import { NotificationSocketService } from 'src/modules/notification/services/notification.socket.service';
 import { NotificationStore } from 'src/modules/notification/notification.store';
+import { NzNotificationService } from 'ng-zorro-antd/notification'
 
 @Component({
   selector: 'app-app-layout',
@@ -23,7 +24,8 @@ export class AppLayoutComponent implements OnInit {
     private _notificationSocketService: NotificationSocketService,
     private socket: WebsocketConnection, 
     private authStore: AuthenticationStore,
-    private _notificationStore: NotificationStore) {
+    private _notificationStore: NotificationStore,
+    private _nzNotification: NzNotificationService) {
 
     this.hasUnreadNotification = false;
   }
@@ -34,6 +36,7 @@ export class AppLayoutComponent implements OnInit {
     this._notificationSocketService.onNewNotification( async (notif) => {
       await this._notificationService.fetch();
       this._notificationStore.appendNotification( notif );
+      this.createNotif( notif );
     } );
     
     this._notificationStore.value$.subscribe( state => {
@@ -47,6 +50,16 @@ export class AppLayoutComponent implements OnInit {
         this.socket.disconnect();
       }
     });
+  }
+
+  createNotif( notification: AnyNotification ) {
+    if( notification.subject === "new_user" ) {
+      this._nzNotification.info('Nouvel arrivant', `${notification.payload.user.username} a rejoint la plateforme !`);
+    } else if( notification.subject === "post_liked" ) {
+      this._nzNotification.info('Nouveau like', `${notification.payload.user.username} a aimé le message suivant : ${notification.payload.preview}`);
+    } else {
+      this._nzNotification.info('Nouvel arrivant', `${notification.payload.user.username} a créé une nouvelle room : ${notification.payload.room.name}`);
+    }
   }
 
   onToggleNotifications() {
